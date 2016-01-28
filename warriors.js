908e3db1e45806468932e455f709626ee6a4cdeb
Aliyah Jessa <aliyah@aliyahmaliyah.com>
2016-01-28 22:19:04 +0000

    Warrior game

---
+++ warriors.js
@@ -0,0 +1,30 @@
+function fight(other) {
+    console.log(this.name + " rushes to the arena with " + other.name)
+}
+function randomPower() {
+    var power = Math.floor((Math.random)*100+1);
+    return power;
+    
+}
+function faceoff(other) {
+    if (this.power > other.power) {
+        console.log("Whoa!" + " " + this.name + " just smoked you, " + other.name + "!");
+    }
+    else if (this.power < other.power) {
+        console.log("You really screwed the pooch on this one, " + this.name + ".");
+    }
+}
+function Warrior(name,gender) {
+        this.name = name;
+        this.gender = gender;
+        this.level = 1;
+        this.weapon = "Wooden Sword";
+        this.power = Math.floor(Math.random()*100+1);
+        this.fight = fight;
+        this.faceoff = faceoff;
+    }
+var warrior1 = new Warrior("aliyah", "F");
+var warrior2 = new Warrior("Sonic", "M");
+warrior1.fight(warrior2);
+warrior1.faceoff(warrior2);
+var warrior3 = new Warrior("mom", "F")
